cca1e9a939039582b9f36283df9600c7e9d226d6
misaraadel <41232116+misaraadel@users.noreply.github.com>
2022-01-27 02:10:23 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+    $(function () {
+        $(document).scroll(function () {
+            var $nav = $(".navbar");
+            $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        });
+    });
+
+    $('.hamburger').click(function(){
+        $('.hamburger').toggleClass('active');
+        $('.navbar-nav').toggleClass('active-nav');
+        $('body').toggleClass('overflowNone')
+    });
+
+    wow = new WOW({
+        boxClass: 'wow',
+        animateClass: 'animated',
+        offset: 200,
+        mobile: true,
+        live: false
+    });
+    wow.init();
+    
+    document.querySelectorAll('.add-to-cart').forEach(button => {
+        button.addEventListener('click', e => {
+            button.classList.toggle('added');
+        });
+    });
+    
+});
